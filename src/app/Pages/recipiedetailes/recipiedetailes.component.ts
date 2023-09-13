3f3abb32db36adeb063c620151413e1280e1e9ee
ethioclicks <ethioclick2020@gmail.com>
2023-09-13 14:03:23 -0500

    design recipe detial page

---
+++ src/app/Pages/recipiedetailes/recipiedetailes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipiedetailes',
+  templateUrl: './recipiedetailes.component.html',
+  styleUrls: ['./recipiedetailes.component.css']
+})
+export class RecipiedetailesComponent {
+
+}
